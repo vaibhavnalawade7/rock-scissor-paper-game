ea83931228a0e9f790cfd164bc977562d7f38ec8
Vaibhav Nalawade <39399049+vaibhavnalawade7@users.noreply.github.com>
2023-12-20 18:10:36 +0530

    Game Main Logic Completed, Remains Hiding Not Pick

---
+++ script.js
@@ -0,0 +1,11 @@
+var num = Math.floor(Math.random() * 3);
+console.log(num)
+if(num == 1) {
+    console.log("Rock")
+}
+else if(num == 2) {
+    console.log("Paper");
+}
+else if(num == 3) {
+    console.log("Scissor")
+}
